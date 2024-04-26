2948cc3ea87a559b34b8b101f1030b7ab596804d
gustonecrush <codeboxezz@gmail.com>
2024-04-26 13:47:11 +0700

    modify sidebar

---
+++ app/admin/[role]/pelatihan/ajukan-pelatihan/page.tsx
@@ -0,0 +1,14 @@
+import DefaultLayout from "@/components/dashboard/Layouts/DefaultLayout";
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <DefaultLayout>
+        <div>AJUKAN PELATIHAN</div>
+      </DefaultLayout>
+    </>
+  );
+}
+
+export default page;
