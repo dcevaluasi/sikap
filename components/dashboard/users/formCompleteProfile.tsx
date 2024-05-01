91480952661c3d154864a713ff937e0c8000c292
gustonecrush <codeboxezz@gmail.com>
2024-05-02 00:59:17 +0700

    add form curriculum vitae work

--- components/dashboard/users/formCompleteProfile.tsx
+++ components/dashboard/users/formCompleteProfile.tsx
@@ -27,6 +27,7 @@ import Image from "next/image";
 import { TbFileStack, TbMapPinSearch, TbUserEdit } from "react-icons/tb";
 
 import { Progress } from "@/components/ui/progress";
+import { MdWorkOutline } from "react-icons/md";
 
 function FormCompleteProfile() {
   const router = useRouter();
@@ -37,6 +38,13 @@ function FormCompleteProfile() {
   const [phoneNumber, setPhoneNumber] = React.useState<string>("");
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // This smooth scrolling is optional, you can remove it if you want instant scrolling
+    });
+  };
 
   /* function to generate data in type FormData */
   const convertDataToFormData = () => {
@@ -543,12 +551,164 @@ function FormCompleteProfile() {
     );
   };
 
-  const FormPemberkasanUser = () => {
+  const FormDataRiwayatPekerjaanUser = () => {
     return (
       <form
         onSubmit={(e) => handleRegistrasiAkun(e)}
         autoComplete="off"
         className={`${indexFormTab == 2 ? "block" : "hidden"}`}
+      >
+        <div className="flex flex-wrap -mx-3 mb-1">
+          <div className="w-full px-3">
+            <label
+              className="block text-gray-800 text-sm font-medium mb-1"
+              htmlFor="email"
+            >
+              Nama Perusahaan Tempat Kerja/Kelompok{" "}
+              <span className="text-red-600">*</span> <br />
+              <span className="text-gray-500">Jika tidak ada isi (-)</span>
+            </label>
+            <input
+              id="phone number"
+              type="text"
+              className="form-input w-full text-black"
+              placeholder="Masukkan Nama Perusahaan/Tempat Kerja"
+              required
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-1">
+          <div className="w-full px-3">
+            <label
+              className="block text-gray-800 text-sm font-medium mb-1"
+              htmlFor="email"
+            >
+              Lokasi Perusahaan Tempat Kerja/Kelompok{" "}
+              <span className="text-red-600">*</span> <br />
+              <span className="text-gray-500">Jika tidak ada isi (-)</span>
+            </label>
+            <input
+              id="phone number"
+              type="text"
+              className="form-input w-full text-black"
+              placeholder="Masukkan Lokasi Perusahaan/Tempat Kerja"
+              required
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-1">
+          <div className="w-full px-3">
+            <label
+              className="block text-gray-800 text-sm font-medium mb-1"
+              htmlFor="email"
+            >
+              Jenis Usaha <span className="text-red-600">*</span>
+              <br />
+              <span className="text-gray-500">Jika tidak ada isi Lainnya</span>
+            </label>
+            <Select>
+              <SelectTrigger className="w-full text-base py-6">
+                <SelectValue placeholder="Pilih Jenis Usaha" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Budidaya">Budidaya</SelectItem>
+                <SelectItem value="Penangkapan">Penangkapan</SelectItem>
+                <SelectItem value="Pengolahan">Pengolahan</SelectItem>
+                <SelectItem value="Lainnya">Lainnya</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-1">
+          <div className="w-full px-3">
+            <label
+              className="block text-gray-800 text-sm font-medium mb-1"
+              htmlFor="email"
+            >
+              Kapasitas Produksi <span className="text-red-600">*</span>
+              <br />
+              <span className="text-gray-500">
+                Isi dalam satuan KG, Jika tidak ada isi (0)
+              </span>
+            </label>
+            <input
+              id="phone number"
+              type="number"
+              className="form-input w-full text-black"
+              placeholder="Masukkan kapasitas produksi"
+              required
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-1">
+          <div className="w-full px-3">
+            <label
+              className="block text-gray-800 text-sm font-medium mb-1"
+              htmlFor="email"
+            >
+              Gaji <span className="text-red-600">*</span> <br />
+              <span className="text-gray-500">Jika tidak ada isi (-)</span>
+            </label>
+            <input
+              id="phone number"
+              type="number"
+              className="form-input w-full text-black"
+              placeholder="Masukkan gaji"
+              required
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-1">
+          <div className="w-full px-3">
+            <label
+              className="block text-gray-800 text-sm font-medium mb-1"
+              htmlFor="email"
+            >
+              Nama Kapal <span className="text-red-600">*</span> <br />
+              <span className="text-gray-500">Jika tidak ada isi (-)</span>
+            </label>
+            <input
+              id="phone number"
+              type="text"
+              className="form-input w-full text-black"
+              placeholder="Masukkan nama kapal"
+              required
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-1">
+          <div className="w-full px-3">
+            <label
+              className="block text-gray-800 text-sm font-medium mb-1"
+              htmlFor="email"
+            >
+              Jenis Alat Tangkap <span className="text-red-600">*</span>
+              <br />
+              <span className="text-gray-500">Jika tidak ada isi Lainnya</span>
+            </label>
+            <Select>
+              <SelectTrigger className="w-full text-base py-6">
+                <SelectValue placeholder="Pilih Jenis Usaha" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Pancing">Pancing</SelectItem>
+                <SelectItem value="Jaring">Jaring</SelectItem>
+                <SelectItem value="Perangkap">Perangkap</SelectItem>
+                <SelectItem value="Lainnya">Lainnya</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </form>
+    );
+  };
+
+  const FormPemberkasanUser = () => {
+    return (
+      <form
+        onSubmit={(e) => handleRegistrasiAkun(e)}
+        autoComplete="off"
+        className={`${indexFormTab == 3 ? "block" : "hidden"}`}
       >
         <div className="flex flex-wrap -mx-3 mb-1">
           <div className="w-full px-3 ">
@@ -651,8 +811,8 @@ function FormCompleteProfile() {
               Lengkapi Data Akun Pengguna E-Laut
             </h1>
             <p className="text-base text-gray-600 max-w-4x">
-              Untuk mempermudah setiap proses daftar pelatihan, lengkapi data
-              dan berkas!
+              Hanya isi sekali bisa digunakan untuk setiap proses pendaftaran,
+              segera lengkapi data-mu!
             </p>
           </div>
 
@@ -662,17 +822,22 @@ function FormCompleteProfile() {
               {indexFormTab == 0 ? (
                 <h2 className="font-bold text-2xl leading-[100%] my-6 md:text-2xl text-black font-calsans flex items-center gap-1">
                   <TbUserEdit />
-                  <span className="mt-2">Lengkapi Data Pribadi</span>
+                  <span className="mt-2">Data Pribadi</span>
                 </h2>
               ) : indexFormTab == 1 ? (
                 <h2 className="font-bold text-2xl leading-[100%] my-6 md:text-2xl text-black font-calsans flex items-center gap-1">
                   <TbMapPinSearch />
-                  <span className="mt-2">Lengkapi Alamat</span>
+                  <span className="mt-2">Alamat Domisili</span>
+                </h2>
+              ) : indexFormTab == 2 ? (
+                <h2 className="font-bold text-2xl leading-[100%] my-6 md:text-2xl text-black font-calsans flex items-center gap-1">
+                  <MdWorkOutline />
+                  <span className="mt-1">Riwayat Pekerjaan</span>
                 </h2>
               ) : (
                 <h2 className="font-bold text-2xl leading-[100%] my-6 md:text-2xl text-black font-calsans flex items-center gap-1">
                   <TbFileStack />
-                  <span className="mt-2">Lengkapi Berkas</span>
+                  <span className="mt-2">Berkas dan Dokumen</span>
                 </h2>
               )}
 
@@ -685,19 +850,24 @@ function FormCompleteProfile() {
                   <span className="font-bold  leading-[100%] my-6 text-blue-500 ">
                     2
                   </span>
-                ) : (
+                ) : indexFormTab == 2 ? (
                   <span className="font-bold  leading-[100%] my-6 text-blue-500 ">
                     3
                   </span>
+                ) : (
+                  <span className="font-bold  leading-[100%] my-6 text-blue-500 ">
+                    4
+                  </span>
                 )}{" "}
-                of 3
+                of 4
               </p>
             </div>
             <div className="flex w-full -mt-2 mb-4">
-              <Progress value={(indexFormTab + 1) * 33.33} max={3} />
+              <Progress value={(indexFormTab + 1) * 25} max={4} />
             </div>
             <FormDataUtamaUser />
             <FormDataAlamatUser />
+            <FormDataRiwayatPekerjaanUser />
             <FormPemberkasanUser />
             <div className="flex -mx-3 mt-5 gap-2 px-3">
               <div className={`w-full ${indexFormTab == 0 && "hidden"}`}>
@@ -706,26 +876,21 @@ function FormCompleteProfile() {
                   className="btn text-white bg-blue-600 hover:bg-blue-700 w-full"
                   onClick={(e) => {
                     setIndexFormTab(indexFormTab - 1);
+                    scrollToTop();
                   }}
                 >
                   Sebelumnya
                 </button>
               </div>
               <div
-                className={`w-full ${indexFormTab == 2 ? "hidden" : "block"}`}
+                className={`w-full ${indexFormTab == 3 ? "hidden" : "block"}`}
               >
                 <button
                   type="submit"
                   className="btn text-white bg-blue-600 hover:bg-blue-700 w-full"
                   onClick={(e) => {
                     setIndexFormTab(indexFormTab + 1);
-                    setFormTab(
-                      indexFormTab == 0
-                        ? "FormDataUtamaUser"
-                        : indexFormTab == 1
-                        ? "FormDataAlamatUser"
-                        : "FormPemberkasanUser"
-                    );
+                    scrollToTop();
                   }}
                 >
                   Selanjutnya
@@ -735,7 +900,7 @@ function FormCompleteProfile() {
                 <AlertDialogTrigger asChild>
                   <div
                     className={`w-full ${
-                      indexFormTab == 2 ? "block" : "hidden"
+                      indexFormTab == 3 ? "block" : "hidden"
                     }`}
                   >
                     <button
@@ -752,13 +917,13 @@ function FormCompleteProfile() {
                       Apakah anda yakin?
                     </AlertDialogTitle>
                     <Image
-                      className="w-2/4 py-4 animate-float"
+                      className="w-[40%] py-4 animate-float"
                       src={"/illustrations/doubt.png"}
                       width={0}
                       height={0}
                       alt="Apakah anda yakin?"
                     />
-                    <AlertDialogDescription className="text-base">
+                    <AlertDialogDescription className="text-base leading-[130%]">
                       Pastikan data anda terisi dengan sesuai dan berkas yang
                       diupload valid dan bener sesuai identitas diri!
                     </AlertDialogDescription>
