04b92ac8e495389afccce2e28b94b8d75d0b3da1
gustonecrush <codeboxezz@gmail.com>
2024-06-12 09:40:29 +0800

    push edit footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -21,101 +21,80 @@ export default function Footer() {
 
           {/* 2nd block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-100 font-medium mb-2">Products</h6>
+            <h6 className="text-gray-100 font-medium mb-2">Balai Pelatihan</h6>
             <ul className="text-sm">
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Web Studio</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">BPPP Medan</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">DynamicBox Flex</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">BDA Sukamandi</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Programming Forms</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">BPPP Tegal</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Integrations</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">BPPP Banyuwangi</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Command-line</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">BPPP Ambon</a>
+              </li>
+              <li className="mb-2">
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">BPPP Bitung</a>
               </li>
             </ul>
           </div>
 
           {/* 3rd block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-100 font-medium mb-2">Resources</h6>
+            <h6 className="text-gray-100 font-medium mb-2">Bidang Pelatihan</h6>
             <ul className="text-sm">
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Documentation</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Budidaya</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Tutorials & Guides</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Kepelautan</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Blog</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Mesin Perikanan</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Support Center</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Penangkapan</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Partners</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Konservasi</a>
               </li>
             </ul>
           </div>
 
           {/* 4th block */}
           <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
-            <h6 className="text-gray-100 font-medium mb-2">Company</h6>
+            <h6 className="text-gray-100 font-medium mb-2">Lembaga/Instansi</h6>
             <ul className="text-sm">
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Home</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">BPPSDM KP</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">About us</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Pusat Pelatihan KP</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Company values</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">DPKAKP</a>
               </li>
               <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Pricing</a>
-              </li>
-              <li className="mb-2">
-                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">Privacy Policy</a>
+                <a href="#0" className="text-gray-200 hover:text-gray-900 transition duration-150 ease-in-out">P2MKP</a>
               </li>
             </ul>
           </div>
 
           {/* 5th block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
-            <h6 className="text-gray-100 font-medium mb-2">Subscribe</h6>
-            <p className="text-sm text-gray-200 mb-4">Get the latest news and articles to your inbox every month.</p>
-            <form>
-              <div className="flex flex-wrap mb-4">
-                <div className="w-full">
-                  <label className="block text-sm sr-only" htmlFor="newsletter">Email</label>
-                  <div className="relative flex items-center max-w-xs">
-                    <input id="newsletter" type="email" className="form-input w-full text-gray-100 px-3 py-2 pr-12 text-sm" placeholder="Your email" required />
-                    <button type="submit" className="absolute inset-0 left-auto" aria-label="Subscribe">
-                      <span className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300" aria-hidden="true"></span>
-                      <svg className="w-3 h-3 fill-current text-blue-600 mx-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                      </svg>
-                    </button>
-                  </div>
-                  {/* Success message */}
-                  {/* <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p> */}
-                </div>
-              </div>
-            </form>
-          </div>
 
-        </div>
+
+        </div >
 
         {/* Bottom area */}
-        <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
+        < div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200" >
 
           {/* Social as */}
-          <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
+          <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0" >
             <li>
               <a href="#0" className="flex justify-center items-center text-gray-200 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
@@ -140,11 +119,11 @@ export default function Footer() {
           </ul>
 
           {/* Copyrights note */}
-          <div className="text-sm text-gray-200 mr-4">&copy; BPPSDM KP. All rights reserved.</div>
+          <div className="text-sm text-gray-200 mr-4" >& copy; BPPSDM KP.All rights reserved.</div>
 
-        </div>
+        </div >
 
-      </div>
-    </footer>
+      </div >
+    </footer >
   )
 }
