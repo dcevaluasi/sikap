947be60a1ad1dee5562ca9a87c0b7025c8d06aec
gustonecrush <codeboxezz@gmail.com>
2024-09-11 11:58:46 +0700

    looking for bug

--- components/user-service.tsx
+++ components/user-service.tsx
@@ -137,9 +137,9 @@ export default function UserService({ user }: { user: User | null }) {
         </div>
       </section>
 
-      {indexMenuSelected == 0 && <UserTrainingService user={user} />}
-      {indexMenuSelected == 1 && <UserCertificateService user={user} />}
-      {indexMenuSelected == 2 && <UserDocuments user={user} />}
+      {/* {indexMenuSelected == 0 && <UserTrainingService user={user} />} */}
+      {/* {indexMenuSelected == 1 && <UserCertificateService user={user} />} */}
+      {/* {indexMenuSelected == 2 && <UserDocuments user={user} />} */}
     </div>
   );
 }
