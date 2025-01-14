65b1c4a677d82ca137541e3b51b28d450142d2c8
gustonecrush <codeboxezz@gmail.com>
2025-01-15 05:18:00 +0700

    deleted not necessary components

--- app/api/admin/admin.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const randomId = Math.random().toString(36).substring(2, 10);
-    res.redirect(307, `/${randomId}/auth/login`);
-}
